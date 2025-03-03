7474af9ea09560c5448e2ea601013949620e4a42
Md Jueal Hossain <mdjueal920977@gmail.com>
2025-03-03 13:56:57 +0600

    fix: purches modal

--- src/components/Product/PurcheaseModal/index.js
+++ src/components/Product/PurcheaseModal/index.js
@@ -9,12 +9,9 @@ const PurchaseModal = ({ data, isOpen, closeModal }) => {
                         className="fixed inset-0 bg-[#0006] dark:bg-white/20"
                         onClick={closeModal}
                     ></div>
-                    <div className=" p-2 bg-white z-50 rounded-lg w-[200px] flex flex-col items-center justify-center">
+                    <div className=" p-2 bg-white z-50 rounded-md w-[150px] flex flex-col items-center justify-center">
                         <div className=" flex items-center justify-center gap-1 mb-1">
-                            <h5 className=" text-H6 text-center font-semibold text-warning_main leading-7 capitalize">
-                                total price:
-                            </h5>
-                            <input className=" text-subtitle2 w-20 outline-none focus:outline-none border rounded px-4 py-[3px]" />
+                            <input className=" text-subtitle2 w-full outline-none focus:outline-none border rounded px-4 py-[3px]" />
                         </div>
                         <div className="w-full mt-2">
                             <button
