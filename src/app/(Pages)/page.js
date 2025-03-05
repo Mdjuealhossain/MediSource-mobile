ec39b23e093f80a8d544f5abe068f1910bb6c9d9
Md Jueal Hossain <mdjueal920977@gmail.com>
2025-03-05 16:00:44 +0600

    fix: filtering

--- src/app/(Pages)/page.js
+++ src/app/(Pages)/page.js
@@ -10,32 +10,6 @@ import Tabs from "@/components/Tabs";
 const Home = () => {
     const { isData, setIsData, isFilterData } = useTab();
     const { presentDay, prevDay } = getFormattedDates(isFilterData?.date);
-
-    // const date =
-    //     isFilterData?.date &&
-    //     new Date(
-    //         isFilterData?.date.year,
-    //         isFilterData?.date.month - 1, // Month is zero-based
-    //         isFilterData?.date.day,
-    //         isFilterData?.date.hour || 0, // Default to 0 if undefined
-    //         isFilterData?.date.minute || 0
-    //     );
-
-    // // Get present day formatted
-    // const presentDayFormatted = date ? date.toISOString().split("T")[0] : null;
-
-    // // Get previous day
-    // const prevDate = date ? new Date(date) : null; // Clone date to avoid mutation
-    // if (prevDate) {
-    //     prevDate.setDate(prevDate.getDate() - 1);
-    // }
-    // const prevDayFormatted = prevDate
-    //     ? prevDate.toISOString().split("T")[0]
-    //     : null;
-
-    // console.log("Present Day:", presentDayFormatted);
-    // console.log("Previous Day:", prevDayFormatted);
-
     const params = {
         district: isFilterData?.district?.id,
         area: isFilterData?.area?.id,
